752707e61d8ad2e1abd0189ebc30668232c82a1d
yashmakhija <yashkrmakhija@gmail.com>
2025-03-10 01:12:55 +0530

    fix: cloudflare url

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
-export const CLOUDFLARE_URL = "https://pub-4dedfd170bbc4cf8bcf66357c32ab64c.r2.dev";
+export const CLOUDFLARE_URL = "https://pub-b2acac8ef6a84c39b35165219b664570.r2.dev";
